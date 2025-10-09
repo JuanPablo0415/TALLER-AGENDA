06cbbe4375abab722bb85d1b1dd6621ea4e3e611
juanp <juanp@LAPTOP-FSE7I7GH>
2025-10-09 13:59:16 -0500

    VERSION 1 INICIAL

---
+++ src/agenda/Agenda.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package agenda;
+
+/**
+ *
+ * @author juanp
+ */
+public class Agenda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        interfaz in=new interfaz();
+        in.setVisible(true);
+    }//fin del main
+    
+}
